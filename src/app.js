9007a3f19d93375a5e608d76c9846bb19495039f
iDiegoggo <diego.garcia.albero@gmail.com>
2026-04-29 14:45:50 -0400

    con parcheo de seguridad

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
+const helmet = require('helmet');
 const app = express();
+app.use(helmet());
+app.disable('x-powered-by');
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "no-store");
+  next();
+});
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
